93c77c91318c7a4036cc2c622f82390238482661
Alexander Loktionov <loktionov129@gmail.com>
2020-04-05 21:07:39 +0300

    feat(mylib): implement some util functions
    DWORD_PTR getModuleAddress(
        const char* moduleName,
        DWORD processId
    );
    
    DWORD getProcessIdByName(
        const char* processName
    );

--- mylib/inc/public/windows/utils.hpp
+++ mylib/inc/public/windows/utils.hpp
@@ -2,12 +2,16 @@
 #define GAMETRAINER_UTILS_HPP
 
 #include <Windows.h>
+#include <TlHelp32.h>
 
 namespace GameTrainer::mylib::windows
 {
     void playSound(const char* sound);
     void setConsoleTitle(const char* title);
     void sleep(size_t ms);
+
+    DWORD_PTR getModuleAddress(const char* moduleName, DWORD processId);
+    DWORD getProcessIdByName(const char* processName);
 }
 
 #endif //GAMETRAINER_UTILS_HPP
--- mylib/src/windows/utils.cpp
+++ mylib/src/windows/utils.cpp
@@ -16,4 +16,55 @@ namespace GameTrainer::mylib::windows
     {
         Sleep(ms);
     }
+
+    DWORD_PTR getModuleAddress(const char *moduleName, const DWORD processId)
+    {
+        MODULEENTRY32 lpModuleEntry = { 0 };
+        HANDLE hSnapShot = CreateToolhelp32Snapshot(TH32CS_SNAPMODULE | TH32CS_SNAPMODULE32, processId);
+
+        if (!hSnapShot)
+            return NULL;
+        lpModuleEntry.dwSize = sizeof(lpModuleEntry);
+        BOOL bModule = Module32First(hSnapShot, &lpModuleEntry);
+        while (bModule)
+        {
+            if (!strcmp(moduleName, lpModuleEntry.szModule))
+            {
+                CloseHandle(hSnapShot);
+                return (DWORD_PTR)lpModuleEntry.modBaseAddr;
+            }
+            bModule = Module32Next(hSnapShot, &lpModuleEntry);
+        }
+        CloseHandle(hSnapShot);
+        return NULL;
+    }
+
+    DWORD getProcessIdByName(const char* processName)
+    {
+        DWORD pid = 0;
+
+        // Create toolhelp snapshot
+        HANDLE snapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+        PROCESSENTRY32 process;
+        ZeroMemory(&process, sizeof(process));
+        process.dwSize = sizeof(process);
+
+        // Walkthrough all processes
+        if (Process32First(snapshot, &process))
+        {
+            do
+            {
+                // Compare process.szExeFile based on format of name, i.e., trim file path
+                // trim .exe if necessary, etc
+                if (!strcmp(processName, process.szExeFile))
+                {
+                    pid = process.th32ProcessID;
+                    break;
+                }
+            } while (Process32Next(snapshot, &process));
+        }
+
+        CloseHandle(snapshot);
+        return pid;
+    }
 }
