aae5756fd6990d789c8ab6249b887a7324edc0ce
Alexander Loktionov <loktionov129@gmail.com>
2020-04-09 19:11:23 +0300

    ci(travis): refactor

--- temp/coveralls.py
+++
@@ -1,8 +0,0 @@
-from sys import path, argv
-path.append('cpp-coveralls')
-
-#from urllib3.exceptions import InsecureRequestWarning
-#requests.packages.urllib3.disable_warnings(category=InsecureRequestWarning)
-
-from cpp_coveralls.__init__ import run
-run()
