e2a9b323762476477bfaed154570ef8bc3c70f68
Alexander Loktionov <loktionov129@gmail.com>
2020-04-12 12:27:51 +0300

    ci(appveyor): fix data copying

--- .ci/zip.bat
+++ .ci/zip.bat
@@ -6,7 +6,7 @@ for /d %%a in (*) do cd "%%~a"
 
 for %%I IN (.) DO set CurrentD=%%~nI%%~xI
 
-robocopy ../../../app/data ./ /E
+robocopy ../../../app/resources/data ./ /E
 
 7z a -tzip GameTrainer-%CurrentD%.zip .
 
