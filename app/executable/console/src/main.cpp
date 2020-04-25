29284da89570273822a4d5eaab0caf008f083d70
Alexander Loktionov <loktionov129@gmail.com>
2020-04-25 14:07:53 +0300

    style(app): fix code style for main.cpp

--- app/executable/console/src/main.cpp
+++ app/executable/console/src/main.cpp
@@ -1,14 +1,14 @@
 #include <gt_core/trainer.hpp>
 
 // TODO: move this logic to gt_console.lib
-int main(int argc, char* argv[])
-{
-    const std::string title = std::string(GT_PROJECT_NAME) + " " + GT_PROJECT_VERSION;
+int main(int argc, char *argv[]) {
+    const std::string title =
+        std::string(GT_PROJECT_NAME) + " " + GT_PROJECT_VERSION;
+
     gt::core::Trainer trainer(title);
 
     // Allow only one instance of application.
-    if (trainer.trainerIsRunning())
-    {
+    if (trainer.trainerIsRunning()) {
         trainer.showOpenedWindow();
 
         return 0;
@@ -18,14 +18,12 @@ int main(int argc, char* argv[])
     system("color a");
 #endif
 
-    if (!trainer.chooseConfig())
-    {
+    if (!trainer.chooseConfig()) {
         return 1;
     }
 
     // Waiting for the game to start ...
-    while (!trainer.gameIsRunning())
-    {
+    while (!trainer.gameIsRunning()) {
         gt::os::sleep(1000);
     }
 
