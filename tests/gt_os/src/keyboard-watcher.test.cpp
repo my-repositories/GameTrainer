940f5507595f0f4565e69f300e27b21debcccee4
Alexander Loktionov <loktionov129@gmail.com>
2020-04-25 22:36:12 +0300

    tests(gt_os): refactor keyboard-watcher::isKeyDown

--- tests/gt_os/src/keyboard-watcher.test.cpp
+++ tests/gt_os/src/keyboard-watcher.test.cpp
@@ -6,41 +6,44 @@
 
 class OsApiMock : public gt::os::OsApi {
   public:
-    MOCK_METHOD(SHORT, getAsyncKeyState, (int vKey), (const, override));
+    MOCK_CONST_METHOD1(getAsyncKeyState, SHORT(int));
 };
 
-TEST(isKeyDown, returns_true_when_VK_F6_pressed) {
-    auto key = VK_F6;
+class isKeyDown : public ::testing::Test {
+  protected:
+    int key{};
     OsApiMock osApiMock;
-    std::vector<int> keysToWatch{key};
-    gt::os::KeyboardWatcher keyboardWatcher(keysToWatch, &osApiMock);
+    gt::os::KeyboardWatcher *keyboardWatcher{};
 
-    ON_CALL(osApiMock, getAsyncKeyState(::testing::_))
-        .WillByDefault(::testing::Return((SHORT)0));
+    void SetUp() override {
+        this->key = VK_F6;
+        std::vector<int> keysToWatch{this->key};
+        this->keyboardWatcher =
+            new gt::os::KeyboardWatcher(keysToWatch, &this->osApiMock);
 
-    EXPECT_CALL(osApiMock, getAsyncKeyState(key))
+        ON_CALL(this->osApiMock, getAsyncKeyState(::testing::_))
+            .WillByDefault(::testing::Return((SHORT)0));
+    }
+
+    void TearDown() override { delete this->keyboardWatcher; }
+};
+
+TEST_F(isKeyDown, returns_true_when_VK_F6_pressed) {
+    EXPECT_CALL(this->osApiMock, getAsyncKeyState(this->key))
         .Times(1)
         .WillOnce(::testing::Return((SHORT)-32767));
 
-    bool actual = keyboardWatcher.isKeyDown(key);
+    bool actual = this->keyboardWatcher->isKeyDown(this->key);
 
     EXPECT_EQ(true, actual);
 }
 
-TEST(isKeyDown, returns_false_when_VK_F6_not_pressed) {
-    auto key = VK_F6;
-    OsApiMock osApiMock;
-    std::vector<int> keysToWatch{key};
-    gt::os::KeyboardWatcher keyboardWatcher(keysToWatch, &osApiMock);
-
-    ON_CALL(osApiMock, getAsyncKeyState(::testing::_))
-        .WillByDefault(::testing::Return((SHORT)0));
-
-    EXPECT_CALL(osApiMock, getAsyncKeyState(key))
+TEST_F(isKeyDown, returns_false_when_VK_F6_not_pressed) {
+    EXPECT_CALL(this->osApiMock, getAsyncKeyState(this->key))
         .Times(1)
         .WillOnce(::testing::Return((SHORT)0));
 
-    bool actual = keyboardWatcher.isKeyDown(key);
+    bool actual = this->keyboardWatcher->isKeyDown(this->key);
 
     EXPECT_EQ(false, actual);
 }
